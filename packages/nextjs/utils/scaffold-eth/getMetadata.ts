4a12897fb7d75e17eb8fbc565d8de0ed88c563af
0x63611e <96933161+naiba4@users.noreply.github.com>
2024-04-24 14:19:18 +0300

    Initialize frames

---
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:${process.env.PORT || 3000}`;
+const titleTemplate = "%s | Scaffold-ETH 2";
+
+export const getMetadata = ({
+  title,
+  description,
+  imageRelativePath = "/thumbnail.jpg",
+}: {
+  title: string;
+  description: string;
+  imageRelativePath?: string;
+}): Metadata => {
+  const imageUrl = `${baseUrl}${imageRelativePath}`;
+
+  return {
+    title: {
+      default: title,
+      template: titleTemplate,
+    },
+    description: description,
+    openGraph: {
+      title: {
+        default: title,
+        template: titleTemplate,
+      },
+      description: description,
+      images: [
+        {
+          url: imageUrl,
+        },
+      ],
+    },
+    twitter: {
+      title: {
+        default: title,
+        template: titleTemplate,
+      },
+      description: description,
+      images: [imageUrl],
+    },
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": imageUrl,
+      "fc:frame:button:1": "Open my 🏗️ Scaffold-ETH 2 dApp",
+      "fc:frame:button:1:action": "link",
+      "fc:frame:button:1:target": baseUrl,
+    },
+  };
+};
